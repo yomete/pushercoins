25b326e79db16450b568c8076e5b39b32fbde17f
Antonio Calapez <antonio@hackages.io>
2017-09-17 00:18:06 +0200

    Fixing the cryptoPrice component

--- src/Today/CryptoPrice.js
+++ src/Today/CryptoPrice.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const CryptoPrice = ({currency, price}) =>
-	<div className="column btc--section">
+	<div className={`column ${currency}--section`}>
 		<h5>${price}</h5>
-		<p>1 {currency}</p>
+		<p>1 {currency.toUpperCase()}</p>
 	</div>;
 
 CryptoPrice.propTypes = {
